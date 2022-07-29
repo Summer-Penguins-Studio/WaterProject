2d09f1f14ba59febd6d152217cd069b062b55864
Carlos Daniel Robaina Rivero <cdrobayna01@gmail.com>
2022-07-28 20:03:39 -0400

    Añadiendo archivos sueltos

--- Assets/Scripts/Player/Camera.cs
+++ Assets/Scripts/Player/Camera.cs
@@ -13,7 +13,7 @@ public class Camera : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Working");
     }
 
     // Update is called once per frame
